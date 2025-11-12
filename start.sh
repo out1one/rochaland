47b0f8766ab24b235d5d16266349b705eadad730
out1one <benjitatrincoso@gmail.com>
2025-11-12 18:33:45 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 MC_VERSION="1.21.10"
 MC_RAM="2G"
-MC_URL="https://launcher.mojang.com/v1/objects/fd5d45a0f0d35d06eaf78a8d4210f9f9de4f3f4b/server.jar"
+MC_URL="https://piston-data.mojang.com/v1/objects/95495a7f485eedd84ce928cef5e223b757d2f764/server.jar"
 
 rm -f server.jar
 echo "Descargando server.jar versión $MC_VERSION..."
@@ -12,6 +12,7 @@ echo "eula=true" > eula.txt
 
 echo "Iniciando Minecraft Server versión $MC_VERSION con $MC_RAM de RAM..."
 java -Xmx$MC_RAM -Xms$MC_RAM -jar server.jar nogui
+
 
 
 
