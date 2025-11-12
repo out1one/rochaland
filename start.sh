920a983eb7b41865fed4a8e282db8737afd1eaf3
out1one <benjitatrincoso@gmail.com>
2025-11-12 18:29:44 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Configuración
 MC_VERSION="1.21.10"
 MC_RAM="2G"
-MC_URL="https://piston-data.mojang.com/v1/objects/95495a7f485eedd84ce928cef5e223b757d2f764/server.jar"
+MC_URL="https://launcher.mojang.com/v1/objects/fd5d45a0f0d35d06eaf78a8d4210f9f9de4f3f4b/server.jar"
 
-# Elimina cualquier server.jar viejo
 rm -f server.jar
-
-# Descargar server.jar oficial si no existe
 echo "Descargando server.jar versión $MC_VERSION..."
-curl -O $MC_URL
+curl -f -O "$MC_URL" || { echo "Error al descargar server.jar"; exit 1; }
 
-# Aceptar EULA automáticamente
 echo "eula=true" > eula.txt
 
-# Iniciar el servidor
 echo "Iniciando Minecraft Server versión $MC_VERSION con $MC_RAM de RAM..."
 java -Xmx$MC_RAM -Xms$MC_RAM -jar server.jar nogui
+
 
 
